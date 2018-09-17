a2d7450e0a45afba4132dea9c04d3741562e22e0
Ismael Hurtado <ismaelhv@outlook.com>
2018-09-16 19:09:01 -0500

    'update'

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const hbs = require('hbs');
+require('./helpers/helpers');
+
+const port = process.env.PORT || 3000; // process.env.PORT: Localmente no existe, sirve cuando se realiza una publicación, y toma el puerto asignado por el server...
+
+app.use(express.static(__dirname + '/public'));
+
+//Express HBS engine
+hbs.registerPartials(__dirname + '/views/parciales/');
+app.set('view engine', 'hbs');
+
+app.get('/', (req, res) => {
+    res.render('home', {
+        nombre: 'ismael hurtado'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        anio: new Date().getFullYear()
+    })
+})
+
+app.listen(3000, () => {
+    console.log(`Escuchando peticiones en el puerto ${port}`);
+})
